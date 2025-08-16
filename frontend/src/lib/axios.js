db9ac705d37096f11ce4b0831346eb257973918d
Guna1301 <gskotipalli2005@gmail.com>
2025-08-16 15:28:19 +0530

    deplyment script changes

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
+const BASE_URL = import.meta.env.MODE === 'development' ? 'http://localhost:5001/api' : 'http://example.com/api';
+
 export const axiosInstance = axios.create({
-    baseURL:"http://localhost:5001/api",
+    baseURL:BASE_URL,
     withCredentials: true, // Include credentials in requests
 })
