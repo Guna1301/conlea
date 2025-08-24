ab580ec4b5b3caf80d0018dde74a096fa903157e
Guna1301 <gskotipalli2005@gmail.com>
2025-08-24 15:26:06 +0530

    backend url updated

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.MODE === 'development' ? 'http://localhost:5001/api' : 'http://example.com/api';
+const BASE_URL = import.meta.env.MODE === 'development' ? 'http://localhost:5001/api': 'https://conlea-backend.onrender.com/api';
 
 export const axiosInstance = axios.create({
     baseURL:BASE_URL,
